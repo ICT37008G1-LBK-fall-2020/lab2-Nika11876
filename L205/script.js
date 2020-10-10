c27ede994c815dea835fbfcbe971f7247265ff4f
Nika11876 <nickolaia777@gmail.com>
2020-10-10 11:43:22 +0400

    add task 5

--- L205/script.js
+++ L205/script.js
@@ -1,12 +1,14 @@
 for (var i = 2; i < 100; i++) {
   var ndiv = 0;
-  for (var j = i - 1; j > 1; j--) {
-    if (i % j == 0) {
-      ndiv++;
-      break;
+  if (i % 2 != 0 || i == 2) {
+    for (var j = i - 1; j > 2; j--) {
+      if (i % j == 0) {
+        ndiv++;
+        break;
+      }
     }
-  }
-  if (ndiv == 0) {
-    alert(i);
+    if (ndiv == 0) {
+      alert(i);
+    }
   }
 }
